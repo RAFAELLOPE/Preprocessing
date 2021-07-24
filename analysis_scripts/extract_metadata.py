72d96991ee6e5c153554267163f99beafa60f6b6
RAFAELLOPE <rafa@LAPTOP-F3TN359K.localdomain>
2021-07-24 17:49:51 +0200

    extract metadata script 2

--- analysis_scripts/extract_metadata.py
+++ analysis_scripts/extract_metadata.py
@@ -37,25 +37,26 @@ def extract_metadata(studies: list):
                             'Spacing Between Slices': list()
                             })
     for s in studies:
-        img = glob.glob(os.path.join(s, '*.dcm'))[0] #Take only the first image in the serie
-        ds = pydicom.dcmread(img, stop_before_pixels=True)
-        metadata['Study_Id'].append(ds[0x0020, 0x0010] if (0x0020, 0x0010) in ds else '')
-        metadata['Serie_Id'].append(ds[0x0020, 0x00E] if (0x0020, 0x00E) in ds else '')
-        metadata['Accession_Number'].append(ds[0x0008, 0x0050] if (0x0008, 0x0050) in ds else '')
-        metadata['Series_Description'].append(ds[0x0008, 0x103E] if (0x0008, 0x103E) in ds else '')
-        metadata['TI (Inversion Time)'].append(ds[0x0018,0x0082] if (0x0018,0x0082) in ds else '')
-        metadata['TR (Repetition Time)'].append(ds[0x0018,0x0080] if (0x0018,0x0080) in ds else None)
-        metadata['TE (Echo Time)'].append(ds[0x0018,0x0081] if (0x0018,0x0081) in ds else None)
-        metadata['Image Plane'].append(image_plane(ds.ImageOrientationPatient))
-        metadata['Study Date'].append(ds[0x0008,0x0020] if (0x0008,0x0020) in ds else None)
-        metadata['MR Adquisition Type'].append(ds[0x0018,0x0023] if (0x0018,0x0023) in ds else '')
-        metadata['Patient Sex'].append(ds[0x0010,0x0040] if (0x0010,0x0040) in ds else '')
-        metadata['Patient Birthday'].append(ds[0x0010,0x0030] if (0x0010,0x0030) in ds else None)
-        metadata['Manufacturer'].append(ds[0x0008,0x0070] if (0x0008,0x0070) in ds else '')
-        metadata['Manufacturers Model Name'].append(ds[0x0008,0x1090] if (0x0008,0x1090) in ds else '')
-        metadata['Magnetic Field Strength'].append(ds[0x0018,0x0087] if (0x0018,0x0087) in ds else None)
-        metadata['Spacing Between Slices'].append(ds[0x0018,0x0088] if (0x0018,0x0088) in ds else None)
-    
+        if len(glob.glob(os.path.join(s, '*.dcm'))) > 0:
+            img = glob.glob(os.path.join(s, '*.dcm'))[0]  #Take only the first image in the serie
+            ds = pydicom.dcmread(img, stop_before_pixels=True)
+            metadata['Study_Id'].append(ds[0x0020, 0x0010].value if (0x0020, 0x0010) in ds else '')
+            metadata['Serie_Id'].append(ds[0x0020, 0x00E].value if (0x0020, 0x00E) in ds else '')
+            metadata['Accession_Number'].append(ds[0x0008, 0x0050].value if (0x0008, 0x0050) in ds else '')
+            metadata['Series_Description'].append(ds[0x0008, 0x103E].value if (0x0008, 0x103E) in ds else '')
+            metadata['TI (Inversion Time)'].append(ds[0x0018,0x0082].value if (0x0018,0x0082) in ds else '')
+            metadata['TR (Repetition Time)'].append(ds[0x0018,0x0080].value if (0x0018,0x0080) in ds else None)
+            metadata['TE (Echo Time)'].append(ds[0x0018,0x0081].value if (0x0018,0x0081) in ds else None)
+            metadata['Image Plane'].append(image_plane(ds[0x0020,0x0037].value) if (0x0020,0x0037) in ds else '')
+            metadata['Study Date'].append(ds[0x0008,0x0020].value if (0x0008,0x0020) in ds else None)
+            metadata['MR Adquisition Type'].append(ds[0x0018,0x0023].value if (0x0018,0x0023) in ds else '')
+            metadata['Patient Sex'].append(ds[0x0010,0x0040].value if (0x0010,0x0040) in ds else '')
+            metadata['Patient Birthday'].append(ds[0x0010,0x0030].value if (0x0010,0x0030) in ds else None)
+            metadata['Manufacturer'].append(ds[0x0008,0x0070].value if (0x0008,0x0070) in ds else '')
+            metadata['Manufacturers Model Name'].append(ds[0x0008,0x1090].value if (0x0008,0x1090) in ds else '')
+            metadata['Magnetic Field Strength'].append(ds[0x0018,0x0087].value if (0x0018,0x0087) in ds else None)
+            metadata['Spacing Between Slices'].append(ds[0x0018,0x0088].value if (0x0018,0x0088) in ds else None)
+
     return pd.DataFrame(metadata)
     
 def manage_arguments():
@@ -63,12 +64,14 @@ def manage_arguments():
     parser.add_argument('--input_directory', 
                         help='Path where dicom files are stored',
                         type=str,
+                        required=True,
                         metavar='-i',
                         default='')
 
     parser.add_argument('--output_file', 
                         help='Path where metadata CSV file is going to be stored',
                         type=str,
+                        required=True,
                         metavar='-o',
                         default='')
     return parser.parse_args()
@@ -76,16 +79,11 @@ def manage_arguments():
 def main(args):
     input_directory = args.input_directory
     output_file = args.output_file
-    metadata = list()
-    assert (os.path.exists(input_directory), "Input directory does not exist")
-    assert (output_file.endswith('.csv'), "Output file should have extension CSV") 
+    assert os.path.exists(input_directory), "Input directory does not exist"
+    assert output_file.endswith('.csv'), "Output file should have extension CSV"
     studies = glob.glob(os.path.join(input_directory, '*', '*'))
     df = extract_metadata(studies)
-        
-        
-
-
-
+    df.to_csv(output_file)
 
 if __name__ == "__main__":
     args = manage_arguments()
